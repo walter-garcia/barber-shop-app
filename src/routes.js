c508a1e0f77f557980ad052d5ce4c4335bdba49d
Walter Marcel Barbosa Garcia <waltermbgarcia@hotmail.com>
2019-09-13 13:05:51 -0300

    Add Schedule controller and mongoose

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -15,14 +16,17 @@ const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
-
+// Global middeware for the next routes.
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
